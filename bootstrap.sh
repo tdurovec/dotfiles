457d727d02ee5d3dff8952dd1586d3fdb5297730
tomowex <37587096+tomowex@users.noreply.github.com>
2022-10-28 16:07:35 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,9 @@ apt-get update && apt-get upgrade -y
 apt-get install software-properties-common
 add-apt-repository ppa:deadsnakes/ppa
 apt-get update
-apt-get install -y python3.10
+apt-get install -y python3.10 curl
+sudo apt-get install python3.10-distutils
+curl -sS https://bootstrap.pypa.io/get-pip.py | python3.10
 
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 2
